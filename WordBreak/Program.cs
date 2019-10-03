76b9afd4fd67e4dc7bf4a1819c5efa34732bb519
Liquid Falme <sansari1@cox.net>
2019-10-03 13:15:18 -0400

    Successfully submited to leetcode, with a minor change I figured out all on my own!

---
+++ WordBreak/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordBreak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String s = "goodtecher";
+            String[] currentDict = { "good", "techer" };
+            Console.WriteLine("My result:   {0}", WordBreakFinder(s, currentDict));
+            Console.ReadLine();
+        }
+        private static bool WordBreakFinder(String s, IList<String> wordDict)
+        {
+            Boolean[] isWordBreak = new Boolean[s.Length + 1];
+
+            isWordBreak[0] = true;
+
+            for (int i = 0; i < s.Length + 1; i++)
+            {
+                for (int j = 0; j < i ; j++)
+                {
+                    if (!isWordBreak[j])
+                    {
+                        continue;
+                    }
+                    if (wordDict.Contains(s.Substring(j, i-j)))
+                    {
+                        isWordBreak[i] = true;
+                        break;
+                    }
+                    }
+            }
+            return isWordBreak[s.Length];
+        }
+    }
+}
